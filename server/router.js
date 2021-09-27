15a7498e2c73c5e4716200fdfac3a7e7d2cd6f96
Antonio Vázquez Cajaraville <antonio.vazquezcajaraville@cognizant.com>
2021-09-27 15:48:33 +0200

    refactor: change promises to async/await

--- server/router.js
+++ server/router.js
@@ -3,17 +3,12 @@ import fs from 'fs';
 
 const router = express.Router();
 
-router.get('/:collection', (req, res, next) => {
-  fs.readFile(
-    `${process.cwd()}/server/data/${req.params.collection}.json`,
-    (err, data) => {
-      if (err) {
-        throw err;
-      }
-      // Simulate slow server response
-      setTimeout(() => res.json(JSON.parse(data)), 1500);
-    }
+router.get('/:collection', async (req, res, next) => {
+  const data = await fs.promises.readFile(
+    `${process.cwd()}/server/data/${req.params.collection}.json`
   );
+  // Simulate slow server response
+  setTimeout(() => res.json(JSON.parse(data)), 1500);
 });
 
 export { router };
